1601c482718eed9e4181906470dcd5538f64cb2d
Pierre Filliolaud <pierre.filliolaud@gmail.com>
2017-03-05 21:51:04 +0100

    add alasql javascript database library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'My First Angular2 App';
+
+  ngOnInit(): void {
+    var title = 'My First Angular2 App!';
+    console.log(title);
+    this.test1();
+    this.test2();
+  }
+
+  test1(): void {
+    var data = [{ a: 1, b: 1, c: 1 }, { a: 1, b: 2, c: 1 }, { a: 1, b: 3, c: 1 }, { a: 2, b: 1, c: 1 }];
+    var res = alasql('SELECT a, COUNT(*) AS b FROM ? GROUP BY a', [data]);
+    console.log(res);
+  }
+
+  test2(): void {
+    var data = [{ a: 1, b: 1, c: 1 }, { a: 1, b: 2, c: 1 }, { a: 1, b: 3, c: 1 }, { a: 2, b: 1, c: 1 }];
+    var res = alasql('SELECT a, COUNT(*) AS b FROM ? GROUP BY a', [data]);
+    console.log(res);
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import * as alasql from 'alasql';
 
 @NgModule({
   declarations: [
